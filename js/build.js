2ca79894ea7d158de1bbbd14ebb49f8aeea18dc6
Alexandr Konopko <8959047@gmail.com>
2018-01-30 13:20:05 +0200

    new file dev portfolio

---
+++ js/build.js
@@ -0,0 +1,4 @@
+console.log('module 1 have done')
+console.log('module 2 have done')
+console.log('module 3 have done')
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1vZHVsZTEuanMiLCJtb2R1bGUyLmpzIiwibW9kdWxlMy5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQ0FBO0FDQUEiLCJmaWxlIjoiYnVpbGQuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnbW9kdWxlIDEgaGF2ZSBkb25lJykiLCJjb25zb2xlLmxvZygnbW9kdWxlIDIgaGF2ZSBkb25lJykiLCJjb25zb2xlLmxvZygnbW9kdWxlIDMgaGF2ZSBkb25lJykiXX0=
